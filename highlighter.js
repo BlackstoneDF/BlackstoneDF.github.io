310835a84c8332ca4f805f6dc27adb3959d18264
akarahdev <118059229+akarahdev@users.noreply.github.com>
2023-05-11 17:54:19 -0400

    make a few changes + test commit

--- highlighter.js
+++ highlighter.js
@@ -4,6 +4,15 @@ let i = elements.length;
 while(i--) {
     let inner = elements[i].innerHTML;
     console.log(inner);
-    inner = inner.replace(/event/g, "<span style='color:red'>event</span>");
+    inner = repl_color(inner, "event", "#ff7777");
+
+    inner = repl_color(inner, "player", "#ff77ff");
+    inner = repl_color(inner, "entity", "#ff77ff");
+    inner = repl_color(inner, "plot", "#ff77ff");
     elements[i].innerHTML = inner;
 };
+
+function repl_color(text, r, color) {
+    console.log(text);
+    return text.replace(new RegExp(`${r}`, "g"), `<span style='color:${color}'>${r}</span>`);
+}
